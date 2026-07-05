d6401f88519ba8097dc0b58c5a8c8a601daf4d56
diegogameroc <gamerocotrinadiego@gmail.com>
2026-07-05 00:54:52 -0500

    cambiosd

--- All_Querys/consultas_fraud_rate.sql
+++ All_Querys/consultas_fraud_rate.sql
@@ -1,3 +1,5 @@
+USE Credit_Card
+
 SELECT*
 FROM dbo.Datos_Usuarios
 
@@ -72,6 +74,45 @@ ROUND(Cant_Fraud*100.00/Total_Transactions,2) AS Fraud_Rate
 FROM CTE_KPI
 ORDER BY Fraud_Rate DESC
 
+
+
+WITH CTE_KPI AS (
+    SELECT 
+        Use_Chip,
+        SUM(CASE 
+                WHEN Is_Fraud = 'Yes' THEN 1
+                ELSE 0
+            END) AS Cant_Fraud,
+        COUNT(*) AS Total_Transactions
+    FROM dbo.Transacciones_Tarjetas
+    GROUP BY Use_Chip
+),
+CTE_FRAUD AS (
+    SELECT
+        Use_Chip,
+        Cant_Fraud,
+        Total_Transactions,
+        ROUND(Cant_Fraud * 100.0 / Total_Transactions, 4) AS Fraud_Rate
+    FROM CTE_KPI
+)
+
+SELECT
+    Use_Chip,
+    Cant_Fraud,
+    Total_Transactions,
+    Fraud_Rate,
+    ROUND(
+        Fraud_Rate /
+        (
+            SELECT Fraud_Rate
+            FROM CTE_FRAUD
+            WHERE Use_Chip = 'Chip Transaction'
+        ),
+        2
+    ) AS Risk_Ratio
+FROM CTE_FRAUD 
+ORDER BY Risk_Ratio DESC;
+ 
 --3. ¿Cómo varía la tasa de fraude según el monto de la transacción?
 
 WITH CTE_KPI AS (
@@ -97,7 +138,7 @@ GROUP BY Categoria_Gasto
 ORDER BY Fraud_Rate DESC;
 
 
---3. ¿Qué estados concentran la mayor tasa de fraude?
+--4. ¿Qué estados concentran la mayor tasa de fraude?
 
 WITH CTE_BASE AS (
     SELECT 
@@ -123,24 +164,82 @@ FROM CTE_BASE
 GROUP BY State
 ORDER BY Fraud_Rate DESC;
 
--- 4. Fraude por merchant (MCC o tipo de comercio)
+-- 5. Fraude por merchant (MCC o tipo de comercio)
 WITH CTE_BASE AS (
     SELECT 
-        t.MCC,
+         MCC,
         CASE 
-            WHEN t.Is_Fraud = 'Yes' THEN 1
+            WHEN Is_Fraud = 'Yes' THEN 1
             ELSE 0
         END AS Fraud_Flag
-    FROM dbo.Transacciones_Tarjetas t
+    FROM dbo.Transacciones_Tarjetas 
 )
 
 SELECT
     MCC,
-    COUNT(*) AS Total_Transactions,
+    COUNT(1) AS Total_Transactions,
     SUM(Fraud_Flag) AS Cant_Fraud,
-    ROUND(SUM(Fraud_Flag) * 100.0 / COUNT(*), 4) AS Fraud_Rate
+    ROUND(SUM(Fraud_Flag) * 100.0 / COUNT(1), 4) AS Fraud_Rate
 FROM CTE_BASE
 GROUP BY MCC
-HAVING COUNT(*) >= 5000
+HAVING COUNT(1) >= 5000
 ORDER BY Fraud_Rate DESC;
+
+
+--6. ¿Cómo varía la tasa de fraude según el nivel de FICO Score de los usuarios?
+
+WITH CTE_BASE AS (
+    SELECT
+    	us.User_ID,
+        us.FICO_Score,
+        CASE 
+            WHEN t.Is_Fraud = 'Yes' THEN 1
+            ELSE 0
+        END AS Fraud_Flag,
+        CASE 
+        	WHEN us.FICO_SCORE <650 THEN 'Riesgo Alto'
+        	WHEN us.FICO_SCORE <=750 THEN 'Riesgo Medio'
+        	ELSE 'Riesgo Bajo' 
+        END AS Segmento_Fico
+    FROM dbo.Transacciones_Tarjetas t
+    INNER JOIN dbo.Datos_Usuarios us
+    ON  us.User_ID = t.User_ID
+)
+SELECT
+	Segmento_Fico,
+	COUNT(1) AS Total_Transactions,
+    SUM(Fraud_Flag) AS Cant_Fraud,
+    ROUND(SUM(Fraud_Flag) * 100.0 / COUNT(1), 4) AS Fraud_Rate
+FROM CTE_BASE
+GROUP BY Segmento_Fico
+ORDER BY Fraud_Rate DESC
+ 
+--7. Cómo varía la tasa de fraude según el Límite de Crédito de los usuarios
+
+WITH CTE_BASE AS (
+    SELECT
+    	CAST(REPLACE(tu.Credit_Limit,'$','') AS FLOAT) Credit_Limit,
+        CASE 
+            WHEN t.Is_Fraud = 'Yes' THEN 1
+            ELSE 0
+        END AS Fraud_Flag,
+        CASE 
+        	WHEN CAST(REPLACE(tu.Credit_Limit,'$','') AS FLOAT) <7043 THEN 'Bajo'
+        	WHEN CAST(REPLACE(tu.Credit_Limit,'$','') AS FLOAT) <=12593 THEN 'Medio'
+        	WHEN CAST(REPLACE(tu.Credit_Limit,'$','') AS FLOAT) <=19157 THEN 'Alto'
+        	ELSE 'Premium' 
+        END AS Segmento_Credito
+    FROM dbo.Transacciones_Tarjetas t
+    INNER JOIN dbo.Tarjetas_Usuarios as tu
+    ON  t.User_ID = tu.User_ID  
+)
+SELECT
+Segmento_Credito,
+COUNT(1) Total_Transactions,
+SUM(Fraud_Flag) Cant_Fraud,
+ROUND(SUM(Fraud_Flag)*100.00/COUNT(1),4) Fraud_Rate
+FROM CTE_BASE
+GROUP BY Segmento_Credito
+ORDER BY Fraud_Rate DESC
+ 
 
